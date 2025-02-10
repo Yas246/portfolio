64c8ff3f8cc5490602f0f1a013a16a6e48a1fb87
Yas246 <bigwabi01@gmail.com>
2025-02-10 22:56:50 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -18,12 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const { i18n } = useTranslation();
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    setIsClient(true);
     const savedLang = localStorage.getItem("language") || "en";
     i18n.changeLanguage(savedLang);
     document.documentElement.lang = savedLang;
   }, [i18n]);
+
+  useEffect(() => {
+    if (isClient) {
+      localStorage.setItem("language", i18n.language);
+      document.documentElement.lang = i18n.language;
+    }
+  }, [i18n.language, isClient]);
 
   return (
     <html lang={i18n.language} suppressHydrationWarning>
